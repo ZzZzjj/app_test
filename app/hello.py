9ddb2a1277ece58dd8e479ee4029e88932f335a6
ZzZzj <zzj1.29@qq.com>
2017-12-18 18:33:01 +0800

    day-1

--- app/hello.py
+++ app/hello.py
@@ -1,5 +1,4 @@
 from flask import Flask,render_template
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,5 +9,9 @@ def home():
 def about():
 	return render_template('about.html')
 
+@app.route('/user/<name>')
+def user(name):
+	return '<h1>Hello,%s!</h1>'% name
+	
 if __name__ == '__main__':
 	app.run(port=8788,debug=True)
