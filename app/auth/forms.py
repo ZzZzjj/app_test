72526063efc187f36b3463592cfa1718de419eaa
ZzZzj <zzj1.29@qq.com>
2017-12-24 23:47:54 +0800

    day_3

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -40,3 +40,13 @@ class PasswordResetForm(Form):
 	password = PasswordField('New password',validators=[DataRequired(),EqualTo('password2',message='Passwords must match')])
 	password2 = PasswordField('Confirm password',validators=[DataRequired()])
 	submit = SubmitField('Reset Password')
+
+class ChangeEmailForm(Form):
+	email = StringField('New email',validators=[DataRequired(),Length(1,64),
+												Email()])
+	password = PasswordField('Password',validators=[DataRequired()])
+	submite = SubmitField('Update Email Address')
+
+	def validate_email(self,field):
+		if User.query.filter_by(email=field.data).first():
+			raise ValidationError('Email already registered.')
--- app/models.py
+++ app/models.py
@@ -1,15 +1,59 @@
-from . import db
+from . import db, login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
-from . import login_manager
-from flask_login import UserMixin
+from flask_login import UserMixin, AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
+from datetime import datetime
+
+class Permission:
+	FOLLOW = 0X01
+	COMMENT = 0X02
+	WRITE_ARTICLES = 0X04
+	MODERATE_COMMENTS = 0X08
+	ADMINISTER = 0X80
 
 class Role(db.Model):
 	__tablename__ = 'roles'
 	id = db.Column(db.Integer,primary_key=True)
 	name = db.Column(db.String(64),unique=True)
+	default = db.Column(db.Boolean, default=False, index=True)
+	permissions = db.Column(db.Integer)
 	users = db.relationship('User',backref='role',lazy='dynamic')
+
+	@staticmethod
+	def insert_roles():
+		roles = {
+			'User': (Permission.FOLLOW |
+					 Permission.COMMENT |
+					 Permission.WRITE_ARTICLES,True),
+			'Moderator': (Permission.FOLLOW |
+						  Permission.COMMENT |
+						  Permission.WRITE_ARTICLES |
+						  Permission.MODERATE_COMMENTS ,False),
+			'Administrator': (0xff,False)
+		}
+		for r in roles:
+			role = Role.query.filter_by(name=r).first()
+			if role is None:
+				role = Role(name=r)
+			role.permissions = roles[r][0]
+			role.default = roles[r][1]
+			db.session.add(role)
+		db.session.commit()
+
+	def add_permission(self, perm):
+		if not self.has_permission(perm):
+			self.permissions += perm
+
+	def remove_permission(self, perm):
+		if self.has_permission(perm):
+			self.permissions -= perm
+
+	def reset_permissions(self):
+		self.permissions = 0
+
+	def has_permission(self,perm):
+		return self.permissions & perm == perm
 
 	def __repr__(self):
 		return '<Role %r>' % self.name
@@ -22,6 +66,19 @@ class User(UserMixin,db.Model):
 	password_hash = db.Column(db.String(128))
 	role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
 	confirmed = db.Column(db.Boolean,default=False)
+	name = db.Column(db.String(64))
+	location = db.Column(db.String(64))
+	about_me = db.Column(db.Text())
+	member_since = db.Column(db.DateTime(),default=datetime.utcnow)
+	last_seen = db.Column(db.DateTime(),default=datetime.utcnow)
+
+	def __init__(self, **kwargs):
+		super(User, self).__init__(**kwargs)
+		if self.role is None:
+			if self.email == current_app.config['FLASKY_ADMIN']:
+				self.role = Role.query.filter_by(permissions=0xff).first()
+			if self.role is None:
+				self.role = Role.query.filter_by(default=True).first()
 
 	@property
 	def password(self):
@@ -68,8 +125,50 @@ class User(UserMixin,db.Model):
 		db.session.add(user)
 		return True
 
+	def generate_email_change_token(self, new_email, expiration=3600):
+		s = Serializer(current_app.config['SECRET_KEY'], expiration)
+		return s.dumps(
+			{'change_email':self.id, 'new_email': new_email}).decode('utf-8')
+
+	def change_email(self, token):
+		s = Serializer(current_app.config['SECRET_KEY'])
+		try:
+			data = s.loads(token.encode('utf-8'))
+		except:
+			return False
+		if data.get('change_email') != self.id:
+			return False
+		new_email = data.get('new_email')
+		if new_email is None:
+			return False
+		if self.query.filter_by(email=new_email).first() is not None:
+			return False
+		self.email = new_email
+		db.session.add(self)
+		return True
+
+	def can(self, permissions):
+		return self.role is not None and (self.role.permissions & permissions) == permissions
+
+	def is_administrator(self):
+		return self.can(Permission.ADMINISTER)
+
 	def __repr__(self):
 		return '<User %r' % self.username
+
+class AnonymousUser(AnonymousUserMixin):
+	def can(self, permissions):
+		return False
+	
+	def is_administrator(self):
+		return False		
+
+	def ping(self):
+		self.last_seen = datetime.utcnow()
+		db.session.add(self)
+
+login_manager.anonymous_user = AnonymousUser
+
 
 @login_manager.user_loader
 def load_user(user_id):
